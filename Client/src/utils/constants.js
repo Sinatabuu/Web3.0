1e9fcb947c5a0689f2602f882b28641ad24aa8b9
Sinatabuu <87429089+Sinatabuu@users.noreply.github.com>
2022-01-05 16:33:38 +0300

    web3 commit

---
+++ Client/src/utils/constants.js
@@ -0,0 +1,8 @@
+import abi from './Transactions.json';
+export const contractABI = abi.abi;
+export  const contractAddress = 
+"0xa3a22dfD9712D4052E6441a8E970674Ea63682FE";
+
+//'0x776e848eFDFb297815eFE9204bCb98075c67aeb1';
+
+//'0xa66d4f6A9992e13461BAC8178d1678f4F27A2efa'
---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,14 @@
+require('@nomiclabs/hardhat-waffle');
+
+module.exports = {
+  solidity: '0.8.0',
+  networks: {
+    ropsten: {
+    url: 'https://eth-ropsten.alchemyapi.io/v2/lCR_ZhvUPz0sHPHrhCCM3eCmLJqtbJUa',
+
+    accounts: ['1ef04c4b5234439867713fbae8a8fc81c6db949107d6ea8cf1f9567a4d0dfe32'],
+
+    },
+
+  },
+};
